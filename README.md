fab32bf5c182b92cfb74efd70f5347b979fd85b7
luka-podgorsek <lp5796@student.uni-lj.si>
2017-02-12 15:15:11 +0100

    Code comments, updated readMe and nodeRed file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# bluemix-mindstorm
+# Bluemix mindstorm
+
+This repository contains code for Lego mindstorm robot and NodeRed application file.
+
+Follow guide below to connect your Lego EV3 robot to Bluemix IoT platform and control your robot using web interface in NodeRed.
+
+Robot will perform different actions according to message you send to it. You can move it, tell it what to say, control its arm and send message back to Bluemix IoT platform with a press of a button.
+
+## Guide
+
+Detailed guide can be found here: [guide url](https://developer.ibm.com/recipes/tutorials/connect-lego-mindstorm-ev3-to-ibm-iot-platform-over-mqtt/)
+
+## Prerequisites
+* [Lego EV3 robot](https://www.lego.com/en-us/mindstorms/products/mindstorms-ev3-31313)
+* [Installed Linux on EV3](http://www.ev3dev.org/docs/getting-started/)
+* [Start IoT platform on Bluemix](https://console.ng.bluemix.net/)
+* Optional [Firefly sensor](https://firefly-iot.com/)
--- ev3.py
+++ ev3.py
@@ -5,14 +5,6 @@ import time
 import ev3dev.ev3 as ev3
 import json
 
-'''
-Organization ID   p38zh6
-Device Type    Lego
-Device ID     LegoEV3
-Authentication Method   token
-Authentication Token   dejan123
-'''
-
 
 class myThread(threading.Thread):
     def __init__(self, thread_id, run_event):
@@ -31,47 +23,50 @@ class myThread(threading.Thread):
 class MindstormBluemix():
     def __init__(self):
         # Ibm Bluemix cloud parameters
-        self.org_id = "p38zh6"
-        self.device_type = "Lego"
-        self.device_name = "LegoEV3"
-        self.token = "dejan123"
-        self.auth_type = "use-token-auth"
-        self.port = 1883
+        self.org_id = "OrgId"  # Your organization ID
+        self.device_type = "Lego"  # Device type (you've defined it on Bluemix iot platform)
+        self.device_name = "LegoEV3"  # Device name (you've defined it on Bluemix iot platform)
+        self.token = "Password"  # Authentication token (you've defined it on Bluemix iot platform)
+        self.auth_type = "use-token-auth"  # Do not change
+        self.port = 1883  # Do not change
         self.subscriber_topic = "iot-2/cmd/get_data/fmt/json"
         self.publisher_topic = "iot-2/evt/mindstorm-status/fmt/json"
 
-        # program mode
-        self.mode = "develop"  # change this to system when using on Mindstorm
+        # Program mode
+        self.mode = "develop"  # change this to system, if you start script on system startup (on robot)
 
-    # format: d:<organisation-id>:<device-type>:<device-name>
+    # Format: d:<organisation-id>:<device-type>:<device-name>
     def make_mqtt_client_info(self):
         return "d:" + self.org_id + ":" + self.device_type + ":" + self.device_name
 
+    # Make connect info
     def make_connect_info(self):
         return self.org_id + ".messaging.internetofthings.ibmcloud.com"
 
-    # Callback for when the client receives a CONNACK response from the server.
+    # Callback when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
         ev3.Sound.speak("I robot connected to Bluemix. Welcome to IBM Innovation center Ljubljana").wait()
 
+    # Callback when robot subscribes to a topic
     def on_subscribe(self, client, userdata, mid, granted_qos):
         print("On subscribe: " + str(mid) + " " + str(granted_qos))
 
+    # Callback when robot publishes a message
     def on_publish(self, client, userdata, mid):
         print("On publish: " + str(mid))
 
-    # The callback for then a PUBLISH message is received from the server.
+    # Callback when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
-        #print("Topic: " + msg.topic + "Payload: " + str(msg.payload))
+        # print("Topic: " + msg.topic + "Payload: " + str(msg.payload))
         # ev3.Sound.speak('Message received').wait()
         handle_message(msg)
 
 
+# Parse received message. Execute action defined in payload
 def handle_message(msg):
     decoded_msg = msg.payload.decode("utf-8")
     payload = json.loads(decoded_msg)
-    # print(payload['voice'])
     if 'voice' in payload:
         ev3.Sound.speak(payload['voice']).wait()
     if 'robotControl' in payload:
@@ -80,6 +75,7 @@ def handle_message(msg):
         move_robot(payload)
 
 
+# Move robot (direction and speed is defined in payload)
 def move_robot(payload):
     if 'direction' and 'speed' and 'time' in payload['moveRobot']:
         speed = int(payload['moveRobot']['speed'])
@@ -115,6 +111,7 @@ def move_robot(payload):
                 time.sleep(0.1)
 
 
+# Move robot arm (Speed and duration is defined in payload)
 def arm_control(payload):
     m = ev3.MediumMotor('outA')
     speed = int(payload['robotControl']['speed'])
@@ -128,6 +125,7 @@ def arm_control(payload):
         print("Motor not connected")
 
 
+# On button press, send payload to Bluemix (MQTT)
 def publish_data(run_event):
     while run_event.is_set():
         ts = ev3.TouchSensor()
@@ -149,13 +147,19 @@ mindBlue = MindstormBluemix()
 if mindBlue.mode != "develop":
     time.sleep(20)
 
+
+# Make new MQTT client
 client = mqtt.Client(mindBlue.make_mqtt_client_info())
+# Define callbacks
 client.on_connect = mindBlue.on_connect
 client.on_message = mindBlue.on_message
 client.on_subscribe = mindBlue.on_subscribe
 client.on_publish = mindBlue.on_publish
+# Set username and password
 client.username_pw_set(mindBlue.auth_type, mindBlue.token)
+# Connect to Bluemix iot platform
 client.connect(mindBlue.make_connect_info(), mindBlue.port, 60)
+# Subscribe to topic
 client.subscribe(mindBlue.subscriber_topic, 0)
 
 
